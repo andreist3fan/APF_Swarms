64e7871bfa6ab10b19a4d03a9d65b04edbc3351e
RensGalesloot <rgalesloot@tudelft.nl>
2025-01-25 12:51:13 +0100

    RAPF is working but still needs some tuning to stop illegal moves

--- agent_algorithms/rapf.py
+++ agent_algorithms/rapf.py
@@ -24,26 +24,26 @@ def pos_update(agent, environment, setup):
                 obstacle_potential += setup.alpha_o * np.exp(-setup.mu_o * distance_squared)
         return target_potential + obstacle_potential
 
-    # Step 3: Set bacteria points, sorted by distance to target
+    # Step 3: Set bacteria points, find the minimum potential
+    N_bacteria_RAPF = 16 ### Change this in setup.py later.
     target_vector_angle = np.arctan((setup.target_y - agent.y)/(setup.target_x - agent.x))
-    bacteria_points = [(agent.x + setup.step_size * np.cos(2* np.pi * k / setup.N_bacteria_RAPF + target_vector_angle), agent.y + setup.step_size * np.sin(2* np.pi * k / setup.N_bacteria_RAPF + target_vector_angle)) for k in range(setup.N_bacteria_RAPF)]
-    def distance_to_target(point):
-        return ((point[0] - setup.target_x)**2 + (point[1] - setup.target_y)**2) **0.5
-    sorted_bacteria_points = sorted(bacteria_points, key=distance_to_target)
+    bacteria_points = [(agent.x + setup.step_size * np.cos(2* np.pi * k / N_bacteria_RAPF + target_vector_angle), agent.y + setup.step_size * np.sin(2* np.pi * k / N_bacteria_RAPF + target_vector_angle)) for k in range(N_bacteria_RAPF)]
+    bacteria_potentials = [potential_field(x, y) for (x, y) in bacteria_points]
+    min_potential = min(bacteria_potentials)
 
-    # Step 4: Find the first bacteria point that has a lower potential and make a step (including random position errors)
-    J_agent = potential_field(agent.x, agent.y)
-    for (x, y) in sorted_bacteria_points:
-        if potential_field(x, y) <= J_agent:
-            new_x = np.random.normal(x, setup.step_variance)
-            new_y = np.random.normal(y, setup.step_variance)
-            return new_x, new_y
-        
-    # Step 5: If no better point was found, the agent is stuck in a local minimum, so an artifical obstacle is placed
-    agent.artificial_obstacles.append((agent.x, agent.y))
-    print("Artificial obstacle placed")
-    print(agent.artificial_obstacles)
-    agent.local_minimum = True
-    return agent.x, agent.y # Return current location to avoid errors
+    # Step 4: Step to the best bacteria point (with random error), or stuck in local minimum
+    if min_potential < potential_field(agent.x, agent.y):
+        selected_point = bacteria_points[bacteria_potentials.index(min_potential)]
+        new_x = np.random.normal(selected_point[0], setup.step_variance)
+        new_y = np.random.normal(selected_point[1], setup.step_variance)
+    else: # Step 5: If no better point was found, the agent is stuck in a local minimum, first try a larger amount of bacteria points, otherwise an artifical obstacle is placed
+        bacteria_points = [(agent.x + setup.step_size * np.cos(2* np.pi * k / setup.N_bacteria), agent.y + setup.step_size * np.sin(2* np.pi * k / setup.N_bacteria)) for k in range(setup.N_bacteria)]
+        bacteria_potentials = [potential_field(x, y) for (x, y) in bacteria_points]
+        min_potential = min(bacteria_potentials)
+        selected_point = bacteria_points[bacteria_potentials.index(min_potential)] # Select the point with lowest potential, even if it is not an improvement (to dodge the artificial obstacle)
+        new_x = np.random.normal(selected_point[0], setup.step_variance)
+        new_y = np.random.normal(selected_point[1], setup.step_variance)
+        if min_potential > potential_field(agent.x, agent.y):
+            agent.artificial_obstacles.append((agent.x, agent.y))
 
-    
+    return new_x, new_y
